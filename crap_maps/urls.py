0f6ef2ddced49ef5f1d56ff91e3a63e80465f2c7
WillG <wcg9ev@virginia.edu>
2023-11-15 12:45:05 -0500

    closed a lot of small issues

--- crap_maps/urls.py
+++ crap_maps/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # path("", views.index, name="index"),
     path('', TemplateView.as_view(template_name="index.html"), name="index"),
     path('accounts/', include('allauth.urls')),
-    path('logout', LogoutView.as_view()),
+    path('logout/', LogoutView.as_view()),
     path('map/', map_view, name='map'),
     path('review/', review_view, name='review'), 
     path('review/success/', TemplateView.as_view(template_name="success.html")),
